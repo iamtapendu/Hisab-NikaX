df115fa53ad410f7dd76a5e04c39b0721c927240
tapendu <tapendukarma@gmail.com>
2025-12-10 17:56:10 +0530

    chnaged name(string 100 to 200)

--- backend/products/model.py
+++ backend/products/model.py
@@ -45,7 +45,7 @@ class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
 
     # Basic details
-    name = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(200), nullable=False)
     description = db.Column(db.Text)
 
     # Pricing
