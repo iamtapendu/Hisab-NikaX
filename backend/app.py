14472f95c73fa7770b52bb0c39f8e61b8a49da7a
tapendu <tapendukarma@gmail.com>
2025-12-10 22:09:54 +0530

    Register product blue print and bug fixed

--- backend/app.py
+++ backend/app.py
@@ -36,7 +36,7 @@ def create_app(config_class=Config):
     """
 
     # Initialize Flask app
-    app = Flask(__name__)
+    app = Flask("Hisab NikaX")
     app.config.from_object(config_class)  # Load configuration from class
 
     # Enable Cross-Origin Resource Sharing (CORS)
@@ -53,6 +53,7 @@ def create_app(config_class=Config):
     # Each blueprint represents a modular component of the app
     from users.routes import users_bp
     from auth.routes import auth_bp
+    from products.routes import products_bp
 
     # from Customer.routes import customer_bp
     # from Supplier.routes import supplier_bp
@@ -65,6 +66,7 @@ def create_app(config_class=Config):
 
     app.register_blueprint(users_bp)
     app.register_blueprint(auth_bp)
+    app.register_blueprint(products_bp)
     # app.register_blueprint(customer_bp)
     # app.register_blueprint(supplier_bp)
     # app.register_blueprint(staff_bp)
--- backend/products/__init__.py
+++ backend/products/__init__.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 
 # name: "users" -> internal identifier for Flask
-products_bp = Blueprint("users", __name__, url_prefix="/api/products")
+products_bp = Blueprint("products", __name__, url_prefix="/api/products")
 
 # Import routes at the end to avoid circular imports
 # This will register all endpoint functions defined in routes.py with this blueprint
