f15b32c15b72a1c745f9f74769f898ad3adedc31
tapendu <tapendukarma@gmail.com>
2026-01-29 22:01:14 +0530

    added user data and global loading flag in the auth provider and refactor the code

--- frontend/src/app/providers/AuthProvider.jsx
+++ frontend/src/app/providers/AuthProvider.jsx
@@ -1,27 +1,59 @@
-import {  useState } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { AuthContext } from "./auth-context";
+import api from "@/lib/api";
 
 export function AuthProvider({ children }) {
-    const [accessToken, setAccessToken] = useState(
-        () => localStorage.getItem("access_token")
-    );
 
-    const login = ({ access_token, refresh_token }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    const logout = useCallback(() => {
+        localStorage.removeItem("access_token");
+        localStorage.removeItem("refresh_token");
+        setUser(null);
+    }, []);
+
+    const fetchUser = useCallback(async () => {
+        try {
+            const { data } = await api.get("/users/profile");
+            setUser(data);
+        } catch (err) {
+            console.log("[Login Failed]", err.message)
+            logout();
+        } finally {
+            setLoading(false);
+        }
+    }, [logout]);
+
+    const login = async ({ access_token, refresh_token }) => {
         localStorage.setItem("access_token", access_token);
         localStorage.setItem("refresh_token", refresh_token);
-        setAccessToken(access_token);
+        await fetchUser();
     };
 
-    const logout = () => {
-        localStorage.clear();
-        setAccessToken(null);
-    };
+    useEffect(() => {
+        async function restoreUserOnRefresh() {
+            const token = localStorage.getItem("access_token");
+            if (!user && token) {
+                await fetchUser();
+            } else setLoading(false);
+        }
+        restoreUserOnRefresh();
+    }, [user, fetchUser]);
+
+    useEffect(() => {
+        const handler = () => logout();
+        window.addEventListener("auth:logout", handler);
+        return () => window.removeEventListener("auth:logout", handler);
+    }, [logout]);
+
 
     return (
         <AuthContext.Provider
             value={{
-                isAuthenticated: !!accessToken,
-                accessToken,
+                isAuthenticated: !!user,
+                user,
+                loading,
                 login,
                 logout,
             }}
