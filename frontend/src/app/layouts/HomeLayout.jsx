3a876c70841ca19219ee037a236d4a22bc46a045
tapendu <tapendukarma@gmail.com>
2026-02-01 15:22:51 +0530

    Added component in place <Header>

--- frontend/src/app/layouts/HomeLayout.jsx
+++ frontend/src/app/layouts/HomeLayout.jsx
@@ -1,40 +1,15 @@
 import SideNavbar from "@/components/SideNavbar";
-import { Outlet, useNavigate } from "react-router-dom"
+import { Outlet } from "react-router-dom"
 import { useState } from "react"
+import Headbar from "@/components/Headbar";
 
 export default function HomeLayout() {
-    let navigate = useNavigate();
     const [sidebarOpen, setSidebarOpen] = useState(true)
 
     return (
         <div className="min-h-screen min-w-screen grid grid-rows-[48px_1fr] m-0 p-0 bg-background">
-            <header className="col-span-2 bg-primary-hv h-12 flex items-center px-4 shadow-md z-50">
-                <div className="flex justify-between items-center w-full" >
-                    <div className="flex items-center gap-2 cursor-pointer"
-                        onClick={() => navigate("/home")}>
-                        <img
-                            src="/logo/logo.png"
-                            alt="Hisab NikaX Logo"
-                            className="h-12 object-contain"
-                        />
-                        <span className="text-2xl font-black text-background">
-                            Hisab NikaX
-                        </span>
-                    </div>
-                    <div className="flex items-center gap-2 cursor-pointer">
-                        <span className="font-bold text-background">
-                            User Account
-                        </span>
-                        {/* TODO attacch user actual profile image */}
-                        <img
-                            src="/logo/user.png"
-                            alt="User profile image"
-                            className="h-8 object-contain rounded-full"
-                        />
 
-                    </div>
-                </div>
-            </header >
+            <Headbar/>
 
             <div
                 className={`grid transition-all duration-300 ease-in-out
