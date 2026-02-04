bde2c6a1cf33474c544901766f622879209d9113
tapendu <tapendukarma@gmail.com>
2026-02-04 19:52:12 +0530

    Updated base layout

--- frontend/src/app/layouts/BaseLayout.jsx
+++ frontend/src/app/layouts/BaseLayout.jsx
@@ -4,23 +4,18 @@ import { useState } from "react"
 import Headbar from "@/components/Headbar";
 
 export default function BaseLayout() {
-    const [sidebarOpen, setSidebarOpen] = useState(true)
-
+    const [sidebarOpen, setSidebarOpen] = useState(true);
     return (
         <div className="min-h-screen min-w-screen grid grid-rows-[48px_1fr] m-0 p-0 bg-background">
-
-            <Headbar/>
-
+            <Headbar />
             <div
                 className={`grid transition-all duration-300 ease-in-out
                 ${sidebarOpen ? "grid-cols-[14rem_1fr]" : "grid-cols-[0_1fr]"}`}
             >
                 <SideNavbar isOpen={sidebarOpen} onToggle={setSidebarOpen} />
-
                 <main className="">
                     <Outlet />
                 </main>
-
             </div>
         </div >
     );
