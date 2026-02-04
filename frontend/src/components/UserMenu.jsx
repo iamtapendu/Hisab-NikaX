78871035bb18f7345dee4c11eae0d3389916477d
tapendu <tapendukarma@gmail.com>
2026-02-04 19:55:25 +0530

    Updated UserMenu

--- frontend/src/components/UserMenu.jsx
+++ frontend/src/components/UserMenu.jsx
@@ -1,8 +1,27 @@
-import { useState } from "react"
+import { memo, useState } from "react"
 import ThemeSwitcher from '@/components/ThemeSwitcher'
+import { useAuth } from "../app/providers/auth-context";
+import { useNavigate } from "react-router-dom";
+import api from "@/lib/api";
+import { logoutUser } from "@/modules/auth/service";
 
-export default function UserMenu({ user }) {
-    const [open, setOpen] = useState(false)
+const UserMenu = memo(function UserMenu() {
+    const [open, setOpen] = useState(false);
+    const { user, logout } = useAuth();
+    const navigate = useNavigate();
+
+    const onLogout = async () => {
+        setOpen(false);
+        await logoutUser();
+        logout();
+        navigate("/auth/login");
+    }
+
+    const onProfile = () => {
+        setOpen(false);
+        navigate("/profile");
+        api.get("/users/profile");
+    }
 
     return (
         <div
@@ -14,11 +33,11 @@ export default function UserMenu({ user }) {
                 className="flex items-center gap-2 cursor-pointer"
                 onClick={() => setOpen(true)}
             >
-                <span className="font-bold text-background">
-                    {user.name}
+                <span className="font-bold text-background min-w-32 text-right capitalize">
+                    {user?.name ?? "User"}
                 </span>
                 <img
-                    src={user.profileImage}
+                    src={user?.profileImage || "/logo/user.png"}
                     alt="User profile"
                     className="h-8 w-8 rounded-full object-cover"
                 />
@@ -31,19 +50,24 @@ export default function UserMenu({ user }) {
                         shadow-foreground border z-50"
                 >
                     <div className="p-4 border-b">
-                        <p className="font-semibold">{user.name}</p>
-                        <p className="text-sm text-foreground/40">{user.username}</p>
+                        <p className="font-semibold capitalize">{user?.name ?? "User"}</p>
+                        <p className="text-sm text-foreground/40">{user?.username ?? "Username"}</p>
                     </div>
                     <ThemeSwitcher />
 
                     <div className="flex flex-col border-t border-foreground/40">
-                        <button className="px-4 py-2 text-left hover:bg-tertiary-hv">
+                        <button className="px-4 py-2 text-left hover:bg-tertiary-hv"
+                            onClick={onProfile}
+                        >
                             Profile
                         </button>
                         <button className="px-4 py-2 text-left hover:bg-tertiary-hv">
                             Settings
                         </button>
-                        <button className="px-4 py-2 text-left hover:bg-tertiary-hv text-red-600">
+                        <button
+                            className="px-4 py-2 text-left hover:bg-tertiary-hv text-danger"
+                            onClick={onLogout}
+                        >
                             Logout
                         </button>
                     </div>
@@ -51,4 +75,6 @@ export default function UserMenu({ user }) {
             )}
         </div>
     )
-}
+});
+
+export default UserMenu;
