b0dd1b2a6a1a96f710c1f73ab6ff6fe89832eb13
tapendu <tapendukarma@gmail.com>
2026-02-05 15:26:28 +0530

    Initial commit for axios interceptors

---
+++ frontend/src/lib/api.js
@@ -0,0 +1,83 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000/api/v1/",
+  timeout: 10000,
+});
+
+api.interceptors.request.use((config) => {
+  const accessToken = localStorage.getItem("access_token");
+  const refreshToken = localStorage.getItem("refresh_token");
+
+  if (["/auth/logout", "/auth/refresh"].some(url => config.url?.includes(url)))
+    config.headers.Authorization = `Bearer ${refreshToken}`;
+  else config.headers.Authorization = `Bearer ${accessToken}`;
+
+  return config;
+},
+  (error) => Promise.reject(error)
+);
+
+let isRefreshing = false;
+let failedQueue = [];
+
+const processQueue = (error, token = null) => {
+  failedQueue.forEach(prom => {
+    error ? prom.reject(error) : prom.resolve(token);
+  });
+  failedQueue = [];
+};
+
+api.interceptors.response.use(
+  response => response,
+  async error => {
+    const originalRequest = error.config;
+
+    if (
+      error.response?.status === 401 &&
+      ["/auth/login", "/auth/refresh"].some(url =>
+        originalRequest.url.includes(url)
+      )
+    ) {
+      return Promise.reject(error);
+    }
+
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      if (isRefreshing) {
+        return new Promise((resolve, reject) => {
+          failedQueue.push({ resolve, reject });
+        }).then(token => {
+          originalRequest.headers.Authorization = `Bearer ${token}`;
+          return api(originalRequest);
+        });
+      }
+
+      originalRequest._retry = true;
+      isRefreshing = true;
+
+      try {
+        const { data } = await api.post("/auth/refresh");
+
+        localStorage.setItem("access_token", data.access_token);
+        localStorage.setItem("refresh_token", data.refresh_token);
+
+        processQueue(null, data.access_token);
+        return api(originalRequest);
+
+      } catch (err) {
+        processQueue(err, null);
+        localStorage.clear();
+        window.dispatchEvent(new Event("auth:logout"));
+        return Promise.reject(err);
+
+      } finally {
+        isRefreshing = false;
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+
+export default api;
