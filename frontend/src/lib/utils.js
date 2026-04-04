87d1f75d81507afad2e036c0a92c5705c3ed1e15
tapendu <tapendukarma@gmail.com>
2026-04-04 11:28:54 +0530

    Created a request handler for services

--- frontend/src/lib/utils.js
+++ frontend/src/lib/utils.js
@@ -1,6 +1,14 @@
-import { clsx } from "clsx";
-import { twMerge } from "tailwind-merge"
+export async function handleRequest(request) {
+    try {
+        const response = await request;
+        return response.data;
+    } catch (error) {
+        if (error.response)
+            throw error.response.data;
 
-export function cn(...inputs) {
-  return twMerge(clsx(inputs));
+        throw {
+            msg: "Network error. Please try again.",
+            errors: error.name + ":" + error.message
+        };
+    }
 }
