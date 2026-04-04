5af3c2c642cbcc358491e6c7cee6c171c8e701dc
tapendu <tapendukarma@gmail.com>
2026-04-04 11:36:10 +0530

    Added all the validations for form fields

--- frontend/src/modules/users/schema.js
+++ frontend/src/modules/users/schema.js
@@ -11,22 +11,38 @@ export const usersSchema = z.object({
         .string()
         .min(8, "Password must be at least 8 characters")
         .max(128, "Password is too long")
-        .regex(/[A-Za-z\d@$!%*?&#^]{8,128}$/, "Password is not valid"),
+        .regex(/[A-Za-z\d@$!%*?&#^]{8,128}$/, "Password is not valid")
+        .optional(),
 
     name: z
-        .string(),
+        .string()
+        .min(3, "Name is too short")
+        .max(50, "Name is too long")
+        .regex(/^[A-Za-z][A-Za-z0-9 ]{3,49}$/, "Name is not valid"),
 
     email: z
-        .string(),
+        .string()
+        .regex(/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/, "Email is not valid")
+        .optional(),
 
     phone: z
-        .string(),
+        .string()
+        .length(10, "Phone Number should be 10 digits")
+        .regex(/^[6-9]\d{9}$/, "Invalid Number")
+        .optional(),
 
-    role:
-        z.string(),
+    role: z
+        .string()
+        .max(10, "Role is too long")
+        .regex(/^(admin|manager|staff|guest)$/, "Invalid Roles")
+        .optional(),
 
-    image:
-        z.string(),
+    image: z
+        .string()
+        .max(50, "Image filename is too long")
+        .regex(/^[A-Za-z0-9_/\-]+\.(jpg|jpeg|png)$/, "Invalid Image")
+        .optional(),
+
 
 
 });
