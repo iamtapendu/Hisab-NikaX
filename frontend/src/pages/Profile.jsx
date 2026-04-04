1fe3629d14013b3fe3aa365d9753f4ffa310f761
tapendu <tapendukarma@gmail.com>
2026-04-04 11:38:42 +0530

    Developed profile page/edit profile page

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,25 +1,52 @@
 import { useAuth } from "@/app/providers/auth-context"
 import { useState } from "react"
-import InputWithPopup from "@/components/InputWithPopup"
 import { usersSchema } from "@/modules/users/schema"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
+import Spinner from "@/components/Spinner"
+import Modal from "@/components/Modal"
+import { updateUser } from "@/modules/users/service"
+import EditableField from "@/components/EditableField"
 
 export default function Profile() {
-    const { user } = useAuth()
-    const [formData, setFormData] = useState(user)
+    const { user, setUser } = useAuth()
     const [isEditing, setIsEditing] = useState(false)
+
+    const [modalMsg, setModalMsg] = useState("");
+    const [modalOpen, setModalOpen] = useState(false);
 
     const {
         register,
         handleSubmit,
+        reset,
         formState: { errors, isSubmitting },
     } = useForm({
         resolver: zodResolver(usersSchema),
+        defaultValues: user,
     });
 
+    const onSubmit = async (values) => {
+        try {
+            const payload = {
+                ...user,
+                ...values,
+            };
+            const updatedUser = await updateUser(user.id, payload);
+            setUser(updatedUser)
+            setModalMsg("Profile updated successfully");
+            setModalOpen(true);
+            setIsEditing(false);
+            console.log("[User Updated]");
+
+        } catch (error) {
+            setModalMsg((error?.msg || "Error") + " " + (error?.errors || ""));
+            setModalOpen(true);
+            console.log("[Update Failed]", error.msg, error.errors);
+        }
+    }
+
     return (
-        <div className="relative grid grid-cols-[30%_70%] m-4">
+        <form className="relative grid grid-cols-[30%_70%] m-4" onSubmit={handleSubmit(onSubmit)}>
             <div
                 className="row-span-6 place-self-center p-2 rounded-full 
                 bg-gradient-to-br from-secondary to-primary-hv shadow-2xl shadow-primary"
@@ -33,67 +60,103 @@ export default function Profile() {
                 </div>
             </div>
 
-            {isEditing ? (
-                <InputWithPopup label= "Username :" name="username" placeholder="Username" defaultValue={user.username} register={register} error={errors.username} />
-            ) : (
-                <span className="font-black text-lg text-foreground/50 place-content-end pl-2">{user.username}</span>
-            )}
-            {isEditing ? (
-                <InputWithPopup label= "Name :" name="name" placeholder="Name" defaultValue={user.name} register={register} error={errors.name} />
-            ) : (
-                <span className="font-black text-5xl capitalize p-2">{user.name}</span>
-            )}
-            {isEditing ? (
-                <InputWithPopup label= "Role :" name="role" placeholder="Role" defaultValue={user.role} register={register} error={errors.role} />
-            ) : (
-                <span className="font-medium text-xl capitalize place-content-center p-2">Role: {user.role}</span>
-            )}
-            {isEditing ? (
-                <InputWithPopup label= "Phone Number :" name="phone" placeholder="Phone Number" defaultValue={user.phone} register={register} error={errors.phone} />
-            ) : (
-                <span className="font-medium text-xl capitalize place-content-center p-2">Phone: {user.phone}</span>
-            )}
-            {isEditing ? (
-                <InputWithPopup label= "Email :" name="email" placeholder="Email" defaultValue={user.email} register={register} error={errors.email} />
-            ) : (
-                <span className="font-medium text-xl capitalize place-content-center p-2">Email: {user.email}</span>
-            )}
-            {isEditing ? (
-                <InputWithPopup label= "Address :" name="address" placeholder="Address" defaultValue={user.address} register={register} error={errors.address} />
-            ) : (
-                <span className="font-medium text-xl capitalize place-content-center p-2">Address: {user.address || "N/A"}</span>
-            )}
+            <EditableField
+                label="Username :"
+                name="username"
+                value={user.username}
+                isEditing={isEditing}
+                register={register}
+                error={errors.username}
+                placeholder={"Username"}
+                className="font-black text-lg text-foreground/50 place-content-end pl-2"
+            />
+
+            <EditableField
+                label="Name :"
+                name="name"
+                value={user.name}
+                isEditing={isEditing}
+                register={register}
+                error={errors.name}
+                placeholder={"Name"}
+                className="font-black text-5xl capitalize p-2"
+            />
+
+            <EditableField
+                label="Role:"
+                name="role"
+                value={user.role}
+                isEditing={isEditing}
+                register={register}
+                error={errors.role}
+                placeholder={"Role"}
+                className="font-medium text-xl capitalize place-content-center p-2"
+                showLabel={true}
+            />
+
+            <EditableField
+                label="Phone:"
+                name="phone"
+                value={user.phone}
+                isEditing={isEditing}
+                register={register}
+                error={errors.phone}
+                placeholder={"Phone"}
+                className="font-medium text-xl capitalize place-content-center p-2"
+                showLabel={true}
+            />
+
+            <EditableField
+                label="Email:"
+                name="email"
+                value={user.email}
+                isEditing={isEditing}
+                register={register}
+                error={errors.email}
+                placeholder={"Email"}
+                className="font-medium text-xl capitalize place-content-center p-2"
+                showLabel={true}
+            />
 
             <button
+                type="button"
                 className="absolute top-4 right-4 btn btn-tertiary h-8 w-8  rounded-full 
                         shadow-lg hover:scale-105 transition-all duration-200 flex items-center 
                         justify-center text-lg"
-                onClick={() => setIsEditing(true)}
+                onClick={() => {
+                    if (!isEditing) {
+                        reset({
+                            username: user.username,
+                            name: user.name,
+                            role: user.role,
+                            phone: user.phone,
+                            email: user.email,
+                            address: user.address
+                        });
+                    }
+                    setIsEditing(prev => !prev);
+                }}
             >
-                &#9998;
+                {!isEditing ? "\u270E" : "\u2715"}
             </button>
 
             {isEditing && (
-                <div className="col-span-2 flex gap-4 mt-4">
+                <div className="absolute bottom-0 right-4 flex gap-4">
                     <button
-                        // onClick={handleSave}
-                        // disabled={loading}
+                        type="submit"
                         className="btn btn-primary"
                     >
-                        Save {/* {loading ? "Saving..." : "Save"} */}
-                    </button>
-
-                    <button
-                        onClick={() => {
-                            setFormData(user);
-                            setIsEditing(false);
-                        }}
-                        className="btn btn-secondary"
-                    >
-                        Cancel
+                        {isSubmitting ? "Saving..." : "Save"}
+                        {isSubmitting && <Spinner />}
                     </button>
                 </div>
             )}
-        </div>
+
+            <Modal
+                open={modalOpen}
+                message={modalMsg}
+                onClose={() => setModalOpen(false)}
+            />
+        </form>
     )
 }
